81a9eb3d80d89a49a6fb33c359e14c5afbdf535b
isaac <isaacsgpz@gmail.com>
2021-07-27 10:37:41 -0300

    script is running

---
+++ script.js
@@ -0,0 +1,60 @@
+const timerDisplay = document.querySelector('#timerDisplay')
+const btnStart = document.querySelector('#start');
+const btnPause = document.querySelector('#pause');
+const btnReset = document.querySelector('#reset');
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let chronometer;
+
+function leftZero(num){
+	return num <= 9 ? `0${num}` : num;
+}
+
+function startCount(){
+	chronometer = setInterval(function(){
+		timerDisplay.style.color = 'green';
+
+		if(seconds < 60){ seconds++;}
+
+		if(seconds == 60){ seconds = 0; minutes++; }
+
+		if(minutes == 60){ minutes = 0; hours++; }
+
+		timerDisplay.innerHTML = `${leftZero(hours)}:${leftZero(minutes)}:${leftZero(seconds)}`;
+
+	}, 1000);
+}
+
+function pauseCount(){
+	setTimeout(function(){
+		clearInterval(chronometer);
+		timerDisplay.style.color = 'red';
+	}, 0);
+}
+
+function resetCount(){
+	setTimeout(function(){
+		clearInterval(chronometer);
+		timerDisplay.style.color = 'black';
+	}, 0);
+
+	seconds =0;
+	minutes = 0;
+	hours = 0;
+
+	timerDisplay.innerHTML = `${leftZero(hours)}:${leftZero(minutes)}:${leftZero(seconds)}`;
+}
+
+btnStart.addEventListener('click', function(event){
+	startCount();
+});
+
+btnPause.addEventListener('click', function(event){
+	pauseCount();
+});
+
+btnReset.addEventListener('click', function(event){
+	resetCount();
+});
